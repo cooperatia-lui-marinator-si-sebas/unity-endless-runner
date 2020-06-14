bb6deec32f189d745c2d81c7c132a2908047490b
sebasLP <65290375+sebasLP@users.noreply.github.com>
2020-06-14 18:06:13 +0200

    Finished

---
+++ Assets/Scripts/MenuBotton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBotton : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
