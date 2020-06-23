b47c448ebe2b703dd3f13c4c62c8b29a1f11aab8
sebasLP <65290375+sebasLP@users.noreply.github.com>
2020-06-23 12:54:54 +0200

    PauseMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Pausemenu, PauseButon;
+
+    public void Pause()
+    {
+        Pausemenu.SetActive(true);
+        PauseButon.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        Pausemenu.SetActive(false);
+        PauseButon.SetActive(true);
+        Time.timeScale = 1;
+    }
+}
