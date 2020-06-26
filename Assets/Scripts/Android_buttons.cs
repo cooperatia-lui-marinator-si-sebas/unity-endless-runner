f94a13babf2d3ba2e763045c0c6091bcd93b5333
marinat0r1 <65226052+marinat0r1@users.noreply.github.com>
2020-06-26 17:14:47 +0200

    added Android Back Button

---
+++ Assets/Scripts/Android_buttons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Android_buttons : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Application.Quit();
+            }
+            else if (SceneManager.GetActiveScene().buildIndex != 2 && SceneManager.GetActiveScene().buildIndex != 4)
+            {
+                SceneManager.LoadScene(0);
+            }
+
+        } 
+    }
+
+}
